497ec0dbc20a2b8019fa1b31ba595dad01ce86cd
박수진 <soojin_expo@naver.com>
2023-03-10 19:10:22 +0900

    Updates

---
+++ _next/static/2ScNj2XWHlZivD8EH_rHz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-57bf820d7510ba97.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],"/main":["static/chunks/pages/main-b8f5266d9055a4f2.js"],sortedPages:["/","/_app","/_error","/main"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
