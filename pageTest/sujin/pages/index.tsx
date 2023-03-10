b7ce487993a04a73cc1bfae089e7edd00d4a92cf
박수진 <soojin_expo@naver.com>
2023-03-10 19:11:14 +0900

    메인페이지

--- pageTest/sujin/pages/index.tsx
+++ pageTest/sujin/pages/index.tsx
@@ -1,15 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] }) 
 
 export default function Home() {
+  
   return (
     <>
-      <div>
-        <p> default pages</p>
+      <div style={{marginTop:20, display:'flex', justifyContent:'center', fontWeight:'bold', fontSize:20}}>
+        <p> 메인 페이지</p>
       </div>
     </>
   )
--- pageTest/sujin/pages/main/index.tsx
+++ pageTest/sujin/pages/main/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const PageInfo = () => {
     return (
         <>
-        <div>pageinfo</div>
+        <div>main page</div>
         </>
     )
 }
